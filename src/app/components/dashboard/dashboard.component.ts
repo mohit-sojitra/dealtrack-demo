6abe097bc5aa937ac4bf2096f1d2a016ff32cc5b
Mohit <mohitsojitraa@gmail.com>
2021-07-06 12:10:19 +0530

    table added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { faCheck, faExclamationCircle, faTasks, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  faUserCircle = faUserCircle;
+  faTasks = faTasks;
+  faCheck = faCheck;
+  faError = faExclamationCircle
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
